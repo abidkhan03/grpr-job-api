c613e317b2f590c8acd7452d120cd54e3cbfc001
abidkhan03 <abidkhan42935@gmail.com>
2023-04-18 12:55:07 +0500

    Rearrange Schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE fetcherjob 
+(
+    id SERIAL PRIMARY KEY,
+    report_title TEXT NOT NULL,
+    search_engine VARCHAR(50),
+    location VARCHAR(250),
+    language VARCHAR(100)
+);
+
 CREATE TABLE combinedjob
 (
     id SERIAL PRIMARY KEY,
@@ -6,15 +15,6 @@ CREATE TABLE combinedjob
     grouping_method VARCHAR(50),
     main_keyword_grouping_accuracy INT,
     variant_keyword_grouping_accuracy INT,
-    search_engine VARCHAR(50),
-    location VARCHAR(250),
-    language VARCHAR(100)
-);
-
-CREATE TABLE fetcherjob 
-(
-    id SERIAL PRIMARY KEY,
-    report_title TEXT NOT NULL,
     search_engine VARCHAR(50),
     location VARCHAR(250),
     language VARCHAR(100)
